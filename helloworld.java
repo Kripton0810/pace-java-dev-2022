013b4f379be575f8abafe4c30fd5fbac579d3fba
Kripton0810 <subhankar0810@gmail.com>
2022-01-23 09:29:10 +0530

    day 1

---
+++ helloworld.java
@@ -0,0 +1,8 @@
+public class helloworld{
+
+    public static void main(String[] args) {
+        double a,b;
+        double sum = (double)22/7;
+        System.out.println("The pi of  is "+sum);
+    }
+}
